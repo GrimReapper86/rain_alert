e9b4a6dcd05a53e4fa76fe60e9589d0e118243f4
marius <ciurel.marius@gmail.com>
2023-09-25 15:09:02 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+from twilio.rest import Client
+
+MY_LAT = 44.376308
+My_LNG = 26.105206
+api_url = 'https://api.openweathermap.org/data/3.0/onecall'
+api_key = "b5430715a3ef9b7a546fba823df195fb"
+account_sid = "ACb910bb57c93fc99052b72b37bc31b49c"
+auth_token = "e9adb9d8c1b232654ef3f5d9f0e4b8f3"
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": My_LNG,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+response = requests.get(url=api_url, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_='+12564725540',
+        body='Bring your umbrella⛈️🌧️☂️',
+        to='+40********'
+    )
+
+    print(message.status)
